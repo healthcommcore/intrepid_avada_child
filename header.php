0c51bbe1595aa3586642c02107efd83436d52f62
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2020-12-09 09:20:05 -0800

    Some homepage formatting, starting on header-landing page styling

--- header.php
+++ header.php
@@ -60,25 +60,7 @@ $wrapper_class .= ( is_page_template( 'blank.php' ) ) ? ' wrapper_blank' : '';
 
 			<?php endif; ?>
 
-
-		<?php if ( has_action( 'avada_override_current_page_title_bar' ) ) : ?>
-			<?php do_action( 'avada_override_current_page_title_bar', $c_page_id ); ?>
-		<?php else : ?>
-    <?php $id = get_post_thumbnail_id($post); ?>
-    <?php $bkgrd_img = wp_get_attachment_url($id); ?>
-			<div class="fusion-page-title-bar">
-        <div class="content-margin">
-          <div class="landing-page-title-bar" style="background-image: url('<?php echo $bkgrd_img; ?>')">
-            <div class="fusion-page-title-row">
-              <div class="fusion-page-title-wrapper">
-                <h1 class="entry-title"><?php the_title() ?></h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-		<?php endif; ?>
-		<?php do_action( 'avada_after_page_title_bar' ); ?>
+			<?php avada_current_page_title_bar( $c_page_id ); ?>
 
 			<?php
 			$row_css    = '';
